2350c30e7b3fbe22fce878821a943d84c192045c
CallMeTrebor <60898829+CallMeTrebor@users.noreply.github.com>
2020-10-02 21:00:33 +0300

    Program

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Threading;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,49 @@ namespace WindowsFormsApp1
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void sendMail()
+		{
+			int i;
+			int counter;
+			var body;
+			var emailAdress;
+			string subject;
+			var title;
+			var recepient;
+			do
+			{
+				var fromAddress = new MailAddress("halorenzo6@gmail.com", title);
+				var toAddress = new MailAddress(emailAdress, recepient);
+				const string fromPassword = "anyuka1212";
+				var smtp = new SmtpClient
+				{
+					Host = "smtp.gmail.com",
+					Port = 587,
+					EnableSsl = true,
+					DeliveryMethod = SmtpDeliveryMethod.Network,
+					UseDefaultCredentials = false,
+					Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+				};
+				using (var message = new MailMessage(fromAddress, toAddress)
+				{
+					Subject = subject,
+					Body = body,
+				})
+				{
+					smtp.Send(message);
+				}
+				counter += 1;
+				i -= 1;
+				Console.WriteLine(b + " emails have been sent");
+				Thread.Sleep(1);
+			} while (i >= 0);
 		}
 	}
 }
